4a95189eed5d03f834194d7a2cb43d4eff9942d4
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-07-01 23:25:07 +0530

    Time: 85 ms (50.75%), Space: 8.3 MB (43.07%) - LeetHub

---
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        priority_queue<int> pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        int ans = 0;
+        while (1) {
+            int top1 = pq.top();
+            pq.pop();
+            int top2 = pq.top();
+            pq.pop();
+
+            if (top1 == 0 || top2 == 0)
+                break;
+            ans++;
+            pq.push(top1 - 1);
+            pq.push(top2 - 1);
+        }
+
+        return ans;
+    }
+};
