a066b95bba467cada883ef9970b88a43f7282e27
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-09-08 18:50:46 +0530

    Time: 323 ms (20.62%), Space: 12.3 MB (98.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
+    // bool solve(int idx, int target, vector<int>& nums) {
+    //     if (target == 0)
+    //         return true;
+    //     if (idx == 0)
+    //         return nums[0] == target;
+
+    //     bool pick = solve(idx - 1, target - nums[idx], nums);
+    //     bool not_pick = solve(idx - 1, target, nums);
+
+    //     return pick || not_pick;
+    // }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        if(nums.size() == 1)    return false;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum & 1)
-            return false;
         int n = nums.size();
-        int t = sum / 2;
-        vector<vector<bool>> dp(n, vector<bool>(t + 1, false));
+        for (int i = 0; i < n; i++)
+            sum += nums[i];
 
-        for (int idx = 0; idx < n; idx++) {
-            dp[idx][0] = true;
-        }
+        if (sum % 2 == 1)
+            return false;
+        int tar = sum / 2;
 
-        // dp[0][nums[0]] = true;
+        vector<vector<bool>> dp(2, vector<bool>(tar + 1, false));
+
+        // bool ans = solve(n - 1, target, nums);
+        // return ans;
+        for (int idx = 0; idx < n; idx++)
+            dp[idx % 2][0] = true;
+        //dp[0][nums[0]] = true;
 
         for (int idx = 1; idx < n; idx++) {
-            for (int target = 1; target <= t; target++) {
-                bool not_pick = dp[idx - 1][target];
+            for (int target = 1; target <= tar; target++) {
                 bool pick = false;
-                if (target >= nums[idx]) {
-                    pick = dp[idx - 1][target - nums[idx]];
-                }
+                if (target >= nums[idx])
+                    pick = dp[(idx - 1) % 2][target - nums[idx]];
+                bool not_pick = dp[(idx - 1) % 2][target];
 
-                dp[idx][target] = pick || not_pick;
+                dp[idx % 2][target] = pick || not_pick;
             }
         }
-        return dp[n - 1][t];
+        return dp[(n - 1) % 2][tar];
     }
 };
