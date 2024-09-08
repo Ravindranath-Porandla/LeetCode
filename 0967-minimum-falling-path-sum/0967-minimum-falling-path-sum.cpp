41dec79c970a327ccd0dd4ae46591e283b543e87
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-09-08 10:32:13 +0530

    Time: 8 ms (88.93%), Space: 13.3 MB (94.83%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,17 +1,52 @@
-#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
+    // int solve(int row, int col, int m, int n, vector<vector<int>>& matrix,
+    //           vector<vector<int>>& dp) {
+    //     if (row < 0 || col < 0 || col >= n || row >= m)
+    //         return 1e9;
+    //     if (row == 0) {
+    //         return matrix[row][col];
+    //     }
+    //     if (dp[row][col] != -1)
+    //         return dp[row][col];
+    //     int up = matrix[row][col] + solve(row - 1, col, m, n, matrix, dp);
+    //     int left_dia =
+    //         matrix[row][col] + solve(row - 1, col - 1, m, n, matrix, dp);
+    //     int right_dia =
+    //         matrix[row][col] + solve(row - 1, col + 1, m, n, matrix, dp);
+
+    //     return dp[row][col] = min(up, min(left_dia, right_dia));
+    // }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        const int M = 1e9 + 7, n = matrix.size();
-        if (n==1) return matrix[0][0];
-        vector<int> dp(n, M), f1;
-        f1=matrix.back();
-        for (int i=n-2; i>=0; i--){
-            for (int j=0; j < n; j++) {
-                dp[j]=matrix[i][j]+min({f1[max(0, j-1)], f1[j], f1[min(n-1, j+1)]});
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(2, vector<int>(n, -1));
+        // int ans = INT_MAX;
+        // for (int i = 0; i < n; i++) {
+        //     ans = min(ans, solve(m - 1, i, m, n, matrix, dp));
+        // }
+
+        for (int col = 0; col < n; col++)
+            dp[0][col] = matrix[0][col];
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                int up = matrix[row][col] + dp[(row - 1) % 2][col];
+                int left_dia = 1e9;
+                if (col - 1 >= 0)
+                    left_dia = matrix[row][col] + dp[(row - 1) % 2][col - 1];
+                int right_dia = 1e9;
+                if (col + 1 < n)
+                    right_dia = matrix[row][col] + dp[(row - 1) % 2][col + 1];
+
+                dp[row % 2][col] = min(up, min(left_dia, right_dia));
             }
-            f1=dp;
         }
-        return *min_element(dp.begin(), dp.end());
+
+        int ans = dp[(m - 1) % 2][0];
+        for (int col = 1; col < n; col++) {
+            ans = min(ans, dp[(m - 1) % 2][col]);
+        }
+        return ans;
     }
 };
