699ee4920664195c91b306ed0a4e2ce881162eab
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-09-11 21:00:23 +0530

    Time: 45 ms (37.19%), Space: 47.7 MB (29.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int solve(int idx, vector<int>& coins, int amount, vector<vector<int>>& dp) {
-        if (idx == 0) {
-            if (amount % coins[0] == 0)
+    int solve(int idx, vector<int>& coins, int amount, vector<vector<int>>& dp){
+        if(idx == 0){
+            if((amount % coins[0]) == 0){
                 return amount / coins[0];
-            else
+            } else {
                 return 1e9;
+            }
         }
 
-        if(dp[idx][amount] != -1)
-            return dp[idx][amount];
+        if(dp[idx][amount] != -1)   return dp[idx][amount];
 
-        int pick = 1e9;
-        if (amount >= coins[idx]) {
-            pick = 1 + solve(idx, coins, amount - coins[idx], dp);
-        }
         int not_pick = solve(idx - 1, coins, amount, dp);
-        return dp[idx][amount] = min(pick, not_pick);
+        int pick = INT_MAX;
+            if(coins[idx] <= amount)
+            pick = 1 + solve(idx, coins, amount - coins[idx], dp);
+        
+        return dp[idx][amount] = min(not_pick, pick);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = solve(coins.size() - 1, coins, amount, dp);
-
-        if (ans >= 1e9)
-            return -1;
-        else
-            return ans;
+        int ans = solve(n - 1, coins, amount, dp);
+        return (ans == 1e9) ? -1 : ans;
     }
 };
