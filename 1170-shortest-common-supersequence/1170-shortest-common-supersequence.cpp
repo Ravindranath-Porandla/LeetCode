6a6680b236b016564b3a6bfe0581eb3af2688520
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-09-15 11:23:52 +0530

    Time: 22 ms (62.87%), Space: 25.8 MB (70.49%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        string ans = "";
+        for (int idx1 = 1; idx1 <= n; idx1++) {
+            for (int idx2 = 1; idx2 <= m; idx2++) {
+                if (str1[idx1 - 1] == str2[idx2 - 1]) {
+                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
+                } else {
+                    dp[idx1][idx2] =
+                        max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1]);
+                }
+            }
+        }
+
+        int idx1 = n;
+        int idx2 = m;
+
+        while (idx1 > 0 && idx2 > 0) {
+            if (str1[idx1 - 1] == str2[idx2 - 1]) {
+                ans.push_back(str1[idx1 - 1]);
+                idx1--;
+                idx2--;
+            } else if (dp[idx1 - 1][idx2] > dp[idx1][idx2 - 1]) {
+                ans.push_back(str1[idx1 - 1]);
+                idx1--;
+            } else {
+                ans.push_back(str2[idx2 - 1]);
+                idx2--;
+            }
+        }
+        while (idx1 > 0) {
+            ans.push_back(str1[idx1 - 1]);
+            idx1--;
+        }
+        while (idx2 > 0) {
+            ans.push_back(str2[idx2 - 1]);
+            idx2--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
