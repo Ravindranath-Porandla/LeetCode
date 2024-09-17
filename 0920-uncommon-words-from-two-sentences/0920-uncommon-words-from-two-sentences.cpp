7093956a6c1eb823f3c44a870a8d872ebcf910c3
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-09-17 20:52:39 +0530

    Time: 5 ms (15.91%), Space: 9 MB (28.22%) - LeetHub

---
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void frequency(string str, unordered_map<string, int>& mp) {
+        stringstream s(str);
+        string word;
+        while (s >> word)
+            mp[word]++;
+    }
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string, int> mp;
+        frequency(s1, mp);
+        frequency(s2, mp);
+        vector<string> ans;
+        for (auto w : mp) {
+            if (w.second < 2) {
+                ans.push_back(w.first);
+            }
+        }
+        return ans;
+    }
+};
