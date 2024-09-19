bf7e39fbbb7691fb2dec27149d7d9b17daaef64e
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-09-19 22:39:27 +0530

    Time: 0 ms (100%), Space: 7.3 MB (59.26%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num == 1)
+            return false;
+        int sum = 0;
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0){
+                sum += i;
+                sum += (num / i);
+            }
+        }
+        cout << sum << endl;
+        return ((sum + 1) == num);
+    }
+};
