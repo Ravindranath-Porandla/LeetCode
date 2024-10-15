5cb099ad789db45afd9a296f9a2917ae32c30951
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-10-15 20:58:04 +0530

    Time: 29 ms (41.88%), Space: 31.5 MB (70.72%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int findSubArrays(vector<int>& nums, int goal) {
+    int solve(vector<int>& nums, int goal) {
         int n = nums.size();
         int left = 0;
         int right = 0;
-        int sum = 0;
+        int ans = 0;
         int cnt = 0;
 
         while (right < n) {
-            sum += nums[right];
+            cnt += nums[right];
 
-            while (sum > goal && left <= right) {
-                sum -= nums[left];
+            while (left <= right && cnt > goal) {
+                cnt -= nums[left];
                 left++;
             }
-
-            cnt += right - left + 1;
+            ans += (right - left + 1);
             right++;
         }
-        return cnt;
+        return ans;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return findSubArrays(nums, goal) - findSubArrays(nums, goal - 1);
+        return solve(nums, goal) - solve(nums, goal - 1);
     }
 };
