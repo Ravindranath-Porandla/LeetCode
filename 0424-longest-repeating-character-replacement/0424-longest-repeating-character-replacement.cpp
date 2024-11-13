69acaf65f63975e45df8869cd6e913569a60ae7f
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-11-13 20:52:22 +0530

    Time: 7 ms (49.11%), Space: 9.9 MB (9.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,17 +4,17 @@ public:
         int n = s.size();
         int left = 0;
         int right = 0;
-        int ans = 0;
+        int ans = INT_MIN;
         unordered_map<char, int> mp;
         int max_count = 0;
 
-        while (right < n) {
+        while(right < n){
             mp[s[right]]++;
             max_count = max(max_count, mp[s[right]]);
 
-            while (left <= right && (right - left + 1 - max_count) > k) {
+            while(left <= right && (right - left + 1 - max_count) > k){
                 mp[s[left]]--;
-                if (mp[s[left]] == 0) {
+                if(mp[s[left]] == 0){
                     mp.erase(s[left]);
                 }
                 left++;
@@ -22,6 +22,7 @@ public:
             ans = max(ans, right - left + 1);
             right++;
         }
+
         return ans;
     }
 };
