139a880c4b8d39fef21ee9cc9286c935752a6d3d
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-12-02 11:28:59 +0530

    Time: 0 ms (100%), Space: 17.1 MB (19.68%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool dfs(int node, int color, vector<int>& visited,
-             vector<vector<int>>& graph) {
-        visited[node] = color;
+    bool check(int currentNode, vector<int>& color, int currentColor,
+               vector<vector<int>>& graph) {
+        color[currentNode] = currentColor;
 
-        for (auto neighbor : graph[node]) {
-            if (visited[neighbor] == -1) {
-                if (dfs(neighbor, !color, visited, graph) == false)
-                    return false;
-            } else if (visited[neighbor] == color) {
+        for (auto& newNode : graph[currentNode]) {
+            if (color[newNode] == currentColor) {
+                return false;
+            }
+            if (color[newNode] == -1 &&
+                check(newNode, color, 1 - currentColor, graph) == false) {
                 return false;
             }
         }
+
         return true;
     }
-
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> visited(n, -1);
+        vector<int> color(n, -1);
 
         for (int i = 0; i < n; i++) {
-            if (visited[i] == -1) {
-                if (dfs(i, 0, visited, graph) == false)
-                    return false;
+            if (color[i] == -1 && check(i, color, 1, graph) == false) {
+                return false;
             }
         }
         return true;
