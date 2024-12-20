8cc6b8d2802b840cbe76955874a3575789ba882a
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-12-20 11:59:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        // if(n <= 1)
+        //     return 1;
+        
+        // return climbStairs(n - 1) + climbStairs(n - 2);
 
-        // Create a vector to store the number of ways to reach each step
-        vector<int> grid(2);
+        // vector<int> dp(n + 1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        int prev2 = 1;
+        int prev1 = 1;
 
-        // Base cases
-        grid[0] = 1; // Ways to reach step 0 (only 1 way: doing nothing)
-        grid[1] = 1; // Ways to reach step 1 (only 1 way: one step of 1)
-
-        // Iteratively calculate the number of ways to reach each step
-        for (int i = 2; i <= n; ++i) {
-            grid[i % 2] = grid[(i - 1) % 2] + grid[(i - 2) % 2];
+        for(int i = 2; i <= n; i++){
+           // dp[i % 2] = dp[(i - 1) % 2] + dp[(i - 2) % 2];
+           int curri = prev1 + prev2;
+           prev2 = prev1;
+           prev1 = curri;
         }
 
-        return grid[n % 2];
+        //return dp[n % 2];
+        return prev1;
     }
 };
