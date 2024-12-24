4836d81c305273aab7895d45d6f16a7ef4083b9c
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2024-12-24 22:44:23 +0530

    Time: 0 ms (100%), Space: 10.9 MB (10.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    // int solve(int idx, vector<int>& nums, vector<int>& dp) {
-    //     if (idx == 0)
-    //         return nums[0];
-    //     if (idx < 0)
-    //         return 0;
+    int solve(int idx, int can_rob, vector<int>& nums, vector<vector<int>>& dp){
+        if(idx == 0){
+            if(can_rob){
+                return nums[0];
+            } else {
+                return 0;
+            }
+        }
 
-    //     if (dp[idx] != -1)
-    //         return dp[idx];
-    //     int rob = nums[idx] + solve(idx - 2, nums, dp);
-    //     int not_rob = solve(idx - 1, nums, dp);
+        if(dp[idx][can_rob] != -1)
+            return dp[idx][can_rob];
 
-    //     return dp[idx] = max(rob, not_rob);
-    // }
+        int profit = INT_MIN;
+
+        if(can_rob){
+            profit = max(nums[idx] + solve(idx - 1, 0, nums, dp), 
+                            solve(idx - 1, 1, nums, dp));
+        } else {
+            profit = solve(idx - 1, 1, nums, dp);
+        }
+
+        return dp[idx][can_rob] = profit;
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(2, -1);
-
-        dp[0] = nums[0];
-        for (int idx = 1; idx < n; idx++) {
-            int rob = nums[idx];
-            if (idx > 1)
-                rob += dp[(idx - 2) % 2];
-            int not_rob = dp[(idx - 1) % 2];
-
-            dp[idx % 2] = max(rob, not_rob);
-        }
-        // int ans = solve(n - 1, nums, dp);
-        // return ans;
-        return dp[(n - 1) % 2];
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solve(n - 1, 1, nums, dp);
     }
 };
