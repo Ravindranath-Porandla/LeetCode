012fe2db494a1b7e54b46d6145817292a06afa54
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-02-27 00:15:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (71%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     // int solve(int idx, vector<int>& nums){
-    //     if(idx == 0)
+    //     if(idx == 0){
     //         return nums[0];
-    //     if(idx < 0)
-    //         return 0;
+    //     }
+
+    //     if(idx == 1){
+    //         return max(nums[0], nums[1]);
+    //     }
 
     //     int rob = nums[idx] + solve(idx - 2, nums);
-    //     int cannot_rob = solve(idx - 1, nums);
+    //     int dont_rob = solve(idx - 1, nums);
 
-    //     return max(rob, cannot_rob);
+    //     return max(rob, dont_rob);
     // }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(2, 0);
-        dp[0] = 0;
-        dp[1] = nums[0];
-
-        for(int idx = 2; idx <= n; idx++){
-            int rob = nums[idx - 1] + dp[(idx - 2) % 2];
-            int cannot_rob = dp[(idx - 1) % 2];
-
-            dp[idx % 2] = max(rob, cannot_rob);
+        if(n == 1){
+            return nums[0];
         }
-        //return solve(n - 1, nums);
+        vector<int> dp(2, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        return dp[n % 2];
+        for (int idx = 2; idx < n; idx++) {
+            int rob = nums[idx] + dp[(idx - 2) % 2];
+            int dont_rob = dp[(idx - 1) % 2];
+
+            dp[idx % 2] = max(rob, dont_rob);
+        }
+        // return solve(n - 1, nums);
+        return dp[(n - 1) % 2];
     }
 };
