23f412782b5b91354fd4c164b72af7e342ee96c1
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-02-28 00:28:48 +0530

    Time: 138 ms (36.81%), Space: 129.5 MB (13.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
-    bool solve(int idx, int target, vector<int>& nums) {
-        if (target == 0)
+    int solve(int idx, vector<int>& nums, int target, vector<vector<int>>& dp) {
+        if (target == 0) {
             return true;
-        if (idx == 0)
-            return nums[0] == target;
+        }
+        if (idx == 0) {
+            return (nums[0] == target);
+        }
 
-        bool pick = solve(idx - 1, target - nums[idx], nums);
-        bool not_pick = solve(idx - 1, target, nums);
+        if(dp[idx][target] != -1){
+            return dp[idx][target];
+        }
 
-        return pick || not_pick;
+        int pick = 0;
+        if (target >= nums[idx]) {
+            pick = solve(idx - 1, nums, target - nums[idx], dp);
+        }
+
+        int not_pick = solve(idx - 1, nums, target, dp);
+
+        return dp[idx][target] = (pick || not_pick);
     }
+
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++)
-            sum += nums[i];
-
-        if (sum % 2 == 1)
-            return false;
-        int tar = sum / 2;
-
-        vector<vector<bool>> dp(n, vector<bool>(tar + 1, false));
-
-        // bool ans = solve(n - 1, target, nums);
-        // return ans;
-        for (int idx = 0; idx < n; idx++)
-            dp[idx][0] = true;
-        if(nums[0] <= tar)
-            dp[0][nums[0]] = true;
-
-        for (int idx = 1; idx < n; idx++) {
-            for (int target = 1; target <= tar; target++) {
-                bool pick = false;
-                if (target >= nums[idx])
-                    pick = dp[(idx - 1)][target - nums[idx]];
-                bool not_pick = dp[(idx - 1)][target];
-
-                dp[idx][target] = pick || not_pick;
-            }
+        for (int num : nums) {
+            sum += num;
         }
-        return dp[(n - 1)][tar];
+
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>((sum / 2) + 1, -1));
+
+        if (sum % 2 != 0) {
+            return 0;
+        }
+
+        return solve(nums.size() - 1, nums, sum / 2, dp);
     }
 };
