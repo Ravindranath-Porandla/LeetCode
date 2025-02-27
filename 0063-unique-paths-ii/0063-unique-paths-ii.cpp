1c55ff9e03dd17cf4858150d9bdd6330cc19fd8f
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-02-27 16:36:44 +0530

    Time: 0 ms (100%), Space: 11.9 MB (28.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
-        
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (row == 0 && col == 0) {
-                    dp[row][col] == 1;
-                } else {
-                    int up = 0;
-                    int left = 0;
-                    if (row >= 1 && obstacleGrid[row][col] != 1)
-                        up = dp[row - 1][col];
-                    if (col >= 1 && obstacleGrid[row][col] != 1)
-                        left = dp[row][col - 1];
+    int solve(int row, int col, vector<vector<int>>& grid){
+        if(row == 0 && col == 0){
+            return 1;
+        }
 
-                    dp[row][col] = up + left;
+        if(row < 0 || col < 0 || grid[row][col] == 1){
+            return 0;
+        }
+
+        int top = solve(row - 1, col, grid);
+        int left = solve(row, col - 1, grid);
+
+        return top + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if(grid[0][0] == 1 || grid[m - 1][n - 1] == 1){
+            return 0;
+        }
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        int col = 0;
+        while(col < n && grid[0][col] == 0){
+            dp[0][col] = 1;
+            col++;
+        }
+
+        int row = 0;
+        while(row < m && grid[row][0] == 0){
+            dp[row][0] = 1;
+            row++;
+        }
+
+        for(int row = 1; row < m; row++){
+            for(int col = 1; col < n; col++){
+                int up = 0;
+                if(grid[row - 1][col] == 0){
+                    up = dp[row - 1][col];
                 }
+                int left = 0;
+                if(grid[row][col - 1] == 0){
+                    left = dp[row][col - 1];
+                }
+
+                dp[row][col] = left + up;
             }
         }
-        return -1 * dp[m - 1][n - 1];
+
+        //return solve(m - 1, n - 1, grid);
+        return dp[m - 1][n - 1];
     }
 };
