51e2024dd34342d180d5be31d708d181860a4ba9
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-02-27 16:52:29 +0530

    Time: 0 ms (100%), Space: 15.6 MB (74.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-    // int solve(int row, int col, int m, int n, vector<vector<int>>& grid) {
-    //     if (row == 0 && col == 0)
-    //         return grid[row][col];
-    //     if (row < 0 || col < 0)
-    //         return 1e9;
+    int solve(int row, int col, vector<vector<int>>& grid) {
+        if (row == 0 && col == 0) {
+            return grid[0][0];
+        }
 
-    //     int up = grid[row][col] + solve(row - 1, col, m, n, grid);
-    //     int left = grid[row][col] + solve(row, col - 1, m, n, grid);
+        int up = INT_MAX;
+        if (row - 1 >= 0)
+            up = grid[row][col] + solve(row - 1, col, grid);
+        int left = INT_MAX;
+        if (col - 1 >= 0)
+            left = grid[row][col] + solve(row, col - 1, grid);
 
-    //     return min(up, left);
-    //}
+        return min(up, left);
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        // int ans = solve(m - 1, n - 1, m, n, grid);
-        // return ans;
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        vector<vector<int>> dp(2, vector<int>(n, 0));
 
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 if (row == 0 && col == 0) {
-                    dp[row][col] = grid[row][col];
-                    continue;
+                    dp[0][0] = grid[0][0];
+                } else {
+                    int up = INT_MAX;
+                    if (row - 1 >= 0)
+                        up = grid[row][col] + dp[(row - 1) % 2][col];
+                    int left = INT_MAX;
+                    if (col - 1 >= 0)
+                        left = grid[row][col] + dp[row % 2][col - 1];
+
+                    dp[row % 2][col] = min(up, left);
                 }
-                int up = 1e9;
-                int left = 1e9;
-                if (row > 0)
-                    up = grid[row][col] + dp[row - 1][col];
-
-                if (col > 0)
-                    left = grid[row][col] + dp[row][col - 1];
-
-                dp[row][col] = min(left, up);
             }
         }
-        return dp[m - 1][n - 1];
+
+        return dp[(m - 1) % 2][n - 1];
+        //return solve(m - 1, n - 1, grid);
     }
 };
