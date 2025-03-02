a7e46f4864c6ed4a0740bd82953ee2bf035aa69f
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-03-02 17:01:13 +0530

    Time: 19 ms (52.36%), Space: 14.4 MB (58.71%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,39 +1,63 @@
 class Solution {
 public:
-    int solve(int idx, vector<int>& nums, int target, vector<vector<int>>& dp,
-              int total_sum) {
-        if (idx == 0) {
-            if (target == nums[0] && target == -nums[0])
-                return 2;
-            return (target == nums[0]) || (target == -nums[0]);
-        }
+    // int solve(int idx, vector<int>& nums, int target, vector<vector<int>>&
+    // dp,
+    //           int total_sum) {
+    //     if (idx == 0) {
+    //         if (target == nums[0] && target == -nums[0])
+    //             return 2;
+    //         return (target == nums[0]) || (target == -nums[0]);
+    //     }
 
-        if ((target + total_sum < 0) ||
-            (target + total_sum >= 2 * total_sum + 1))
-            return 0;
+    //     if ((target + total_sum < 0) ||
+    //         (target + total_sum >= 2 * total_sum + 1))
+    //         return 0;
 
-        if (dp[idx][target + total_sum] != -1) {
-            return dp[idx][target + total_sum];
-        }
+    //     if (dp[idx][target + total_sum] != -1) {
+    //         return dp[idx][target + total_sum];
+    //     }
 
-        int plus = solve(idx - 1, nums, target - nums[idx], dp, total_sum);
-        int minus = solve(idx - 1, nums, target + nums[idx], dp, total_sum);
+    //     int plus = solve(idx - 1, nums, target - nums[idx], dp, total_sum);
+    //     int minus = solve(idx - 1, nums, target + nums[idx], dp, total_sum);
 
-        return dp[idx][target + total_sum] = plus + minus;
-    }
+    //     return dp[idx][target + total_sum] = plus + minus;
+    // }
 
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWays(vector<int>& nums, int t) {
         int n = nums.size();
         int total_sum = accumulate(nums.begin(), nums.end(), 0);
 
-        // If target is out of the possible sum range, return 0
-        if (abs(target) > total_sum)
+        if (abs(t) > total_sum)
             return 0;
 
-        // Memoization Table (Shift index by `total_sum` to handle negative
-        // targets)
-        vector<vector<int>> dp(n, vector<int>(2 * total_sum + 1, -1));
+        vector<vector<int>> dp(2, vector<int>(2 * total_sum + 1, 0));
 
-        return solve(n - 1, nums, target, dp, total_sum);
+        // return solve(n - 1, nums, target, dp, total_sum);
+
+        for (int target = -total_sum; target <= total_sum; target++) {
+            if (target == nums[0] && target == -nums[0])
+                dp[0][target + total_sum] = 2;
+            else if ((target == nums[0]) || (target == -nums[0])) {
+                dp[0][target + total_sum] = 1;
+            }
+        }
+
+        for (int idx = 1; idx < n; idx++) {
+            for (int target = -total_sum; target <= total_sum; target++) {
+                int plus = 0, minus = 0;
+
+                if ((target - nums[idx]) + total_sum >= 0 &&
+                    (target - nums[idx]) + total_sum < 2 * total_sum + 1)
+                    plus = dp[(idx - 1) % 2][(target - nums[idx]) + total_sum];
+
+                if ((target + nums[idx]) + total_sum >= 0 &&
+                    (target + nums[idx]) + total_sum < 2 * total_sum + 1)
+                    minus = dp[(idx - 1) % 2][(target + nums[idx]) + total_sum];
+
+                dp[idx % 2][target + total_sum] = plus + minus;
+            }
+        }
+
+        return dp[(n - 1) % 2][t + total_sum];
     }
 };
