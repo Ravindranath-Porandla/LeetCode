709be7b7d94786c12232dbe238b5d21eac73f87c
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-03-04 08:32:09 +0530

    Time: 23 ms (78.88%), Space: 9.2 MB (92.29%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    // int solve(int idx1, int idx2, string text1, string text2, vector<vector<int>>& dp){
-    //     if(idx1 == 0 || idx2 == 0){
+    // int solve(int idx1, int idx2, string text1, string text2) {
+    //     if (idx1 == 0 || idx2 == 0) {
     //         return 0;
     //     }
 
-    //     if(dp[idx1][idx2] != -1)
-    //         return dp[idx1][idx2];
-    //     if(text1[idx1 - 1] == text2[idx2 - 1]){
-    //         return dp[idx1][idx2] = 1 + solve(idx1 -1, idx2 - 1, text1, text2, dp);
+    //     int take = 0;
+    //     if (text1[idx1 - 1] == text2[idx2 - 1]) {
+    //         take = 1 + solve(idx1 - 1, idx2 - 1, text1, text2);
     //     }
-    //                 return dp[idx1][idx2] = max(solve(idx1 - 1, idx2, text1, text2, dp), 
-    //                 solve(idx1, idx2 - 1, text1, text2, dp));
 
+    //     int not_take = max(solve(idx1 - 1, idx2, text1, text2),
+    //                        solve(idx1, idx2 - 1, text1, text2));
+
+    //     return max(take, not_take);
     // }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        // int ans = solve(n, m, text1, text2, dp);
-        // return ans;
+        //return solve(n, m, text1, text2);
 
-        for(int idx1 = 0; idx1 <= n; idx1++)
-            dp[idx1][0] = 0;
-        for(int idx2 = 0; idx2 <= m; idx2++)
-            dp[0][idx2] = 0;
-        
-        for(int idx1 = 1; idx1 <=n; idx1++){
-            for(int idx2 = 1; idx2 <= m; idx2++){
-                if(text1[idx1 - 1] == text2[idx2 - 1]){
-                     dp[idx1][idx2] = 1 + dp[idx1 -1][idx2 - 1];
-                } else {
-                    dp[idx1][idx2] = max(dp[idx1 - 1][idx2], 
-                    dp[idx1][idx2 - 1]);
+        vector<vector<int>> dp(2, vector<int>(m + 1, 0));
+
+        for (int idx1 = 1; idx1 <= n; idx1++) {
+            for (int idx2 = 1; idx2 <= m; idx2++) {
+                int take = 0;
+                if (text1[idx1 - 1] == text2[idx2 - 1]) {
+                    take = 1 + dp[(idx1 - 1) % 2][idx2 - 1];
                 }
+
+                int not_take = max(dp[(idx1 - 1) % 2][idx2],
+                                   dp[idx1 % 2][idx2 - 1]);
+
+                dp[idx1 % 2][idx2] = max(take, not_take);
             }
         }
-        return dp[n][m];
+
+        return dp[n % 2][m];
     }
 };
