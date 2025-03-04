70dd1e05b0259d63a10e9d3071381b7c3069a8ad
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-03-04 20:51:35 +0530

    Time: 33 ms (7.54%), Space: 26.7 MB (82.83%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
+    string shortestCommonSupersequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
 
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
         for (int idx1 = 1; idx1 <= n; idx1++) {
             for (int idx2 = 1; idx2 <= m; idx2++) {
-                if (str1[idx1 - 1] == str2[idx2 - 1]) {
-                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
-                } else {
-                    dp[idx1][idx2] =
-                        max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1]);
+                int take = 0;
+                if (text1[idx1 - 1] == text2[idx2 - 1]) {
+                    take = 1 + dp[idx1 - 1][idx2 - 1];
                 }
+
+                int not_take = max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1]);
+
+                dp[idx1][idx2] = max(take, not_take);
             }
         }
-
-        string ans = "";
 
         int idx1 = n;
         int idx2 = m;
+        string ans = "";
+
         while (idx1 > 0 && idx2 > 0) {
-            if (str1[idx1 - 1] == str2[idx2 - 1]) {
-                ans.push_back(str1[idx1 - 1]);
+            if (text1[idx1 - 1] == text2[idx2 - 1]) {
+                ans.push_back(text1[idx1 - 1]);
                 idx1--;
                 idx2--;
-            } else if (dp[idx1][idx2 - 1] > dp[idx1 - 1][idx2]) {
-                ans.push_back(str2[idx2 - 1]);
-                idx2--;
+            } else if (dp[idx1 - 1][idx2] > dp[idx1][idx2 - 1]) {
+                ans.push_back(text1[idx1 - 1]);
+                idx1--;
             } else {
-                ans.push_back(str1[idx1 - 1]);
-                idx1--;
+                ans.push_back(text2[idx2 - 1]);
+                idx2--;
             }
         }
-
         while (idx1 > 0) {
-            ans.push_back(str1[idx1 - 1]);
+            ans.push_back(text1[idx1 - 1]);
             idx1--;
         }
         while (idx2 > 0) {
-            ans.push_back(str2[idx2 - 1]);
+            ans.push_back(text2[idx2 - 1]);
             idx2--;
         }
-
         reverse(ans.begin(), ans.end());
         return ans;
     }
