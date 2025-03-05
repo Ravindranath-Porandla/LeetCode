d114eca87367c5dd0f5c832275998c4023321977
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-03-05 23:10:56 +0530

    Time: 0 ms (100%), Space: 8.4 MB (74.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    // int solve(int row, int col, int m, int n, vector<vector<int>>& dp) {
-    //     if (row == 0 && col == 0)
+    // int solve(int i, int j, int m, int n, vector<vector<int>>& dp) {
+    //     if (i == 0 && j == 0)
     //         return 1;
-    //     if (row < 0 || row >= m || col < 0 || col >= n)
-    //         return 0;
 
-    //     if (dp[row][col] != -1)
-    //         return dp[row][col];
-    //     int up = solve(row - 1, col, m, n, dp);
-    //     int left = solve(row, col - 1, m, n, dp);
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
 
-    //     return dp[row][col] = up + left;
+    //     int left = 0;
+    //     if (j - 1 >= 0)
+    //         left = solve(i, j - 1, m, n, dp);
+    //     int up = 0;
+    //     if (i - 1 >= 0)
+    //         up = solve(i - 1, j, m, n, dp);
+
+    //     return dp[i][j] = left + up;
     // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(2, vector<int>(n, 1));
-        // int ans = solve(m - 1, n - 1, m, n, dp);
-        // return ans;
+        vector<vector<int>> dp(2, vector<int>(n, 0));
+        // return solve(m - 1, n - 1, m , n, dp);
 
-        for (int row = 1; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                int up = dp[(row - 1) % 2][col];
-                int left = dp[row % 2][col - 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 && j == 0){
+                    dp[0][0] = 1;
+                    continue;
+                }
+                int left = 0;
+                if (j - 1 >= 0)
+                    left = dp[i % 2][j - 1];
+                int up = 0;
+                if (i - 1 >= 0)
+                    up = dp[(i - 1) % 2][j];
 
-                dp[row % 2][col] = up + left;
+                dp[i % 2][j] = left + up;
             }
         }
 
