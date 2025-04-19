fdd10211a839e11779984ea0820be3b92285ed28
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-04-19 17:20:42 +0530

    Time: 67 ms (16.91%), Space: 60.4 MB (38.74%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,14 +1,63 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& v, int lower, int upper) {
-        long long ans = 0;
-        sort(v.begin(), v.end());
+    int LB(vector<int>& vec, int target, int start) {
+        int low = start;
+        int high = vec.size() - 1;
+        int res = -1;
 
-        for (int i = 0; i < v.size() - 1; i++) {
-            auto up = upper_bound(v.begin() + i + 1, v.end(), upper - v[i]);
-            auto low = lower_bound(v.begin() + i + 1, v.end(), lower - v[i]);
-            ans += (up - low);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (vec[mid] == target) {
+                res = mid;
+                high = mid - 1;
+            } else if (vec[mid] > target) {
+                res = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
+
+        return res;
+    }
+
+    int UB(vector<int>& vec, int target, int start) {
+        int low = start;
+        int high = vec.size() - 1;
+        int res = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (vec[mid] == target) {
+                res = mid;
+                low = mid + 1;
+            } else if (vec[mid] > target) {
+                high = mid - 1;
+            } else {
+                res = mid;
+                low = mid + 1;
+            }
+        }
+
+        return res;
+    }
+
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        //vector<int> vec;
+        sort(nums.begin(), nums.end());
+        long long int ans = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            // nums[j] >= lower -  nums[i]
+            int p1 = LB(nums, lower - nums[i], i + 1);
+            // nums[j] <= upper - nums[i]
+            int p2 = UB(nums, upper - nums[i], i + 1);
+            if (p1 != -1 && p2 != -1 && p2 >= p1) {
+                ans += (p2 - p1 + 1);
+            }
+        }
+
         return ans;
     }
 };
