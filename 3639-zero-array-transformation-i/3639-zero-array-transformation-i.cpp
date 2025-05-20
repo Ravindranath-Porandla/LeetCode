be5f15d997368b6dd4d50abc21484cd789444d31
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-05-20 11:32:53 +0530

    Time: 2 ms (90.04%), Space: 292.8 MB (86.91%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> line_sweep(n, 0);
+
+        for (auto& query : queries) {
+            int l = query[0];
+            int r = query[1];
+
+            if (r < n - 1)
+                line_sweep[r + 1]--;
+            line_sweep[l]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                line_sweep[i] = line_sweep[i] + line_sweep[i - 1];
+            if ((nums[i] - line_sweep[i]) > 0)
+                return false;
+        }
+
+        return true;
+    }
+};
