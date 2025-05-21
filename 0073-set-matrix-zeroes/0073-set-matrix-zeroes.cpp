6343497bb282efcb573ce33002c25742cc8d8d27
Ravindranath-Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-05-21 12:22:13 +0530

    Time: 0 ms (100%), Space: 18.6 MB (52.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,21 +4,21 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
+        vector<bool> rows(n, false);
+        vector<bool> cols(n, false);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    rows[i] = true;
+                    cols[j] = true;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (row[i] == 1 || col[j] == 1) {
+                if (rows[i] || cols[j]) {
                     matrix[i][j] = 0;
                 }
             }
