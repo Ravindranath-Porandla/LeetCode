d5b94cf21bdbfeaf836220b6015c1d9f7cbe13a7
Ravindranath Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-07-23 20:19:07 +0530

    Time: 19 ms (94.65%), Space: 31.3 MB (30.41%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    string solve(string s, string matchString) {
-        int i = 0;
-        for (int j = 0; j < s.size(); j++) {
-            s[i] = s[j];
-            i++;
-            if (i >= 2 && s[i - 2] == matchString[0] &&
-                s[i - 1] == matchString[1])
-                i -= 2;
+    pair<int, string> solve(string& s, string temp, int points) {
+        int char1 = temp[0];
+        int char2 = temp[1];
+        string modified;
+        int res = 0;
+
+        for (char ch : s) {
+            if (modified.size() && ch == char2 && modified.back() == char1) {
+                res += points;
+                modified.pop_back();
+            } else {
+                modified.push_back(ch);
+            }
         }
 
-        s.erase(s.begin() + i, s.end());
-        return s;
+        return {res, modified};
     }
     int maximumGain(string s, int x, int y) {
-        string maxString = (x > y) ? "ab" : "ba";
-        string minString = (x < y) ? "ab" : "ba";
-        int ans = 0;
-        string temp1 = solve(s, maxString);
-        int len = s.size() - temp1.size();
-        ans += (len / 2) * max(x, y);
-        string temp2 = solve(temp1, minString);
-        len = temp1.size() - temp2.size();
-        ans += (len / 2) * min(x, y);
-        return ans;
+        int res = 0;
+        if (x >= y) {
+            auto a = solve(s,"ab", x);
+            res += a.first;
+            auto b = solve(a.second, "ba", y);
+            res += b.first;
+        } else {
+            auto a = solve(s, "ba", y);
+            res += a.first;
+            auto b = solve(a.second, "ab", x);
+            res += b.first;
+        }
+
+        return res;
     }
 };
