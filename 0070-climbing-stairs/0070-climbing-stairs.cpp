3c8afc33e353af051af5fcb40b31ab3c6e163747
Ravindranath Porandla <70263810+Ravindranath-Porandla@users.noreply.github.com>
2025-12-07 22:48:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // if(n <= 1)
-        //     return 1;
-        
-        // return climbStairs(n - 1) + climbStairs(n - 2);
+        vector<int> dp(2, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int idx = 2; idx <= n; idx++) {
+            int one_step = dp[(idx - 1) % 2];
+            int two_step = dp[(idx - 2) % 2];
 
-        // vector<int> dp(n + 1);
-        // dp[0] = 1;
-        // dp[1] = 1;
-        int prev2 = 1;
-        int prev1 = 1;
-
-        for(int i = 2; i <= n; i++){
-           // dp[i % 2] = dp[(i - 1) % 2] + dp[(i - 2) % 2];
-           int curri = prev1 + prev2;
-           prev2 = prev1;
-           prev1 = curri;
+            dp[idx % 2] = one_step + two_step;
         }
-
-        //return dp[n % 2];
-        return prev1;
+        return dp[n % 2];
     }
 };
